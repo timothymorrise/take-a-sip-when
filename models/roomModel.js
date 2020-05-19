32cbac9084a31210428dd273de24fa5aa428c725
Timothy Morrise <timothy.morrise@gmail.com>
2020-05-19 17:03:33 -0600

    working on Room Schema

--- models/roomModel.js
+++ models/roomModel.js
@@ -1,4 +1,22 @@
 // roomModel.js
 // ==============================
 
-//
+// imports/instantiate Schema
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+// Schema
+const clientIDSchema = new Schema({
+    id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+})
+
+const roomSchema = new Schema({
+    clients: [
+        clientIDSchema
+    ]
+})
+
+// exports
